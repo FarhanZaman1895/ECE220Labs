8f2dfb41e75411d73dbe4ebe01d4f8a5198b3e95
Farhan Zaman <farhanzaman1895@gmail.com>
2025-03-09 10:53:13 -0600

    Lab 3 code (not correct)

---
+++ Lab 3/src/main.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include "io.h"
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+#define BUFFER_SIZE 64
+
+#define MIN     0
+#define MAX     1
+#define ODD     2
+#define EVEN    3
+#define MINMAX  4
+#define ODDEVEN 5
+
+// prototypes of your functions you need to write
+
+// this function fills input buffer using values
+// obtained with the function get_value()
+void reading(int[]);
+
+// this function performs processing of data in local buffer
+// and put results back into local buffer
+void processing(int[]);
+
+// this function transfers data between two buffers
+// it is used to transfer data from input buffer to local buffer
+// and to transfer data from local buffer to output buffer
+void transferring(int[], int[]);
+
+// this function submit the results stored
+// in output buffer for validation
+void submitting(int[]);
+
+// Functions for calculating max, min, # of odds, # of evens
+int min(int localBuffer[]);
+int max(int localBuffer[]);
+int odds(int localBuffer[]);
+int evens(int localBuffer[]);
+
+// two functions provided to you (defined in io.h and io.c) are:
+// int get_value(void);
+//       this function generates a single value
+//       you use it to obtain this value and put it into an input buffer
+// int submit_result(int[]);
+//       you use this function to submit the data processing results
+//       for validation (your results are checked inside this function)
+
+
+int main(void) {
+    
+    int inputBuffer [BUFFER_SIZE]   = {0};
+    int localBuffer [BUFFER_SIZE]   = {0};
+    int outputBuffer [BUFFER_SIZE]  = {0};
+
+    reading(inputBuffer);
+
+    while (inputBuffer[0] != -1) {
+        transferring(inputBuffer, localBuffer);
+        processing(localBuffer);
+        transferring(localBuffer, outputBuffer);
+        submitting(outputBuffer);
+        // set all buffers to 0
+        reading(inputBuffer);
+    }
+    return 0;
+}
+
+void reading(int inputBuffer[]) {
+    inputBuffer[0] = get_value();
+    inputBuffer[1] = get_value();
+    for (int i = 0; i < inputBuffer[1]; i++) {
+        inputBuffer[i + 2] = get_value();
+    }
+}
+
+void transferring(int sourceBuffer[], int destinationBuffer[]) {
+    for (int i = 0; i < sourceBuffer[1] + 2; i++) {
+        destinationBuffer[i] = sourceBuffer[i];
+    }
+}
+
+void processing(int localBuffer[]) {
+    localBuffer[1] = (localBuffer[0] >= 4) + 1;
+    switch (localBuffer[0]) {
+        case 0:
+            localBuffer[2] = min(localBuffer);
+        case 1:
+            localBuffer[2] = max(localBuffer);
+        case 2:
+            localBuffer[2] = odds(localBuffer);
+        case 3:
+            localBuffer[2] = evens(localBuffer);
+        case 4:
+            localBuffer[2] = min(localBuffer);
+            localBuffer[3] = max(localBuffer);
+        case 5:
+            localBuffer[2] = odds(localBuffer);
+            localBuffer[3] = evens(localBuffer);
+    }
+}
+
+
+void submitting(int outputBuffer[]) {
+    if (submit_results(outputBuffer)) {
+        printf("The results were not correct.\n");
+    } else {
+        printf("The results were correct.\n");
+    }
+}
+
+int min(int localBuffer[]) {
+    int min = localBuffer[2];
+
+    for (int i = 0; i < localBuffer[1]; i++) {
+        if (localBuffer[i + 2] < min) {
+            min = localBuffer[i + 2];
+        }
+    }
+
+    return min;
+}
+
+int max(int localBuffer[]) {
+    int max = localBuffer[2];
+
+    for (int i = 0; i < localBuffer[1]; i++) {
+        if (localBuffer[i + 2] > max) {
+            max = localBuffer[i + 2];
+        }
+    }
+
+    return max;
+}
+
+int odds(int localBuffer[]) {
+    int odds = 0;
+
+    for (int i = 0; i < localBuffer[1]; i++) {
+        if (localBuffer[i + 2] % 2 == 1) {
+            odds++;
+        }
+    }
+
+    return odds;
+}
+
+int evens(int localBuffer[]) {
+    int evens = 0;
+
+    for (int i = 0; i < localBuffer[1]; i++) {
+        if (localBuffer[i + 2] % 2 == 0) {
+            evens++;
+        }
+    }
+
+    return evens;
+}
